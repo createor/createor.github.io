f3fc3e6acc19fb2e478ea789fa9afec7d1e5fd3f
createor <40543429+createor@users.noreply.github.com>
2023-03-14 11:18:45 +0800

    Update items.js

--- items.js
+++ items.js
@@ -4,7 +4,7 @@
 items = [{
     "name": "ChatGPT",
     "desc": "使用python语言对接chatgpt的api实现的网页版",
-    "bg_img": "./ai.png",
+    "bg_img": "./icon/ai.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/ChatGPT",
     "fork": 0,
@@ -12,7 +12,7 @@ items = [{
 },{
     "name": "百度翻译插件",
     "desc": "谷歌浏览器扩展插件--百度翻译",
-    "bg_img": "./extensions.png",
+    "bg_img": "./icon/extensions.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/translate_extensions",
     "fork": 0,
@@ -20,7 +20,7 @@ items = [{
 },{
     "name": "股票桌面端",
     "desc": "使用python的PyQt库开发的实时监测股票的桌面端程序",
-    "bg_img": "./gupiao.png",
+    "bg_img": "./icon/gupiao.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/gupiao",
     "fork": 0,
@@ -28,7 +28,7 @@ items = [{
 },{
     "name": "监控键盘输入",
     "desc": "使用python监控键盘输入,过滤/禁止发送关键词汇",
-    "bg_img": "./keyboard.png",
+    "bg_img": "./icon/keyboard.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/monitorKeyboard",
     "fork": 0,
@@ -36,7 +36,7 @@ items = [{
 },{
     "name": "采集PL/SQL使用的sql命令",
     "desc": "使用golang编写的记录PL/SQL客户端发送的sql命令",
-    "bg_img": "./oracle.png",
+    "bg_img": "./icon/oracle.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/oracle_syslog",
     "fork": 0,
@@ -44,7 +44,7 @@ items = [{
 },{
     "name": "手机自动签到",
     "desc": "使用python编写的通过adb自动操作手机签到",
-    "bg_img": "./auto.png",
+    "bg_img": "./icon/auto.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/autoRegister",
     "fork": 0,
@@ -52,7 +52,7 @@ items = [{
 },{
     "name": "服务器信息收集",
     "desc": "使用golang编写的收集服务器信息并上报告警",
-    "bg_img": "./server.png",
+    "bg_img": "./icon/server.png",
     "type": 1, // 0-public公开,1-private私有
     "url": "https://github.com/createor/monitor",
     "fork": 0,
