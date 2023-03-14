835746470fc5c773bab8cd7350cef3941c2fec5b
createor <40543429+createor@users.noreply.github.com>
2023-03-14 11:16:38 +0800

    Update items.js

--- items.js
+++ items.js
@@ -4,25 +4,57 @@
 items = [{
     "name": "ChatGPT",
     "desc": "使用python语言对接chatgpt的api实现的网页版",
-    "bg-img": "项目背景图片",
+    "bg_img": "./ai.png",
     "type": 1, // 0-public公开,1-private私有
-    "url": "https://github.com/createor/monitorKeyboard",
-    "fork": 100,
-    "star": 4
-},{
-    "name": "测试项目2",
-    "desc": "项目描述9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999",
-    "bg-img": "项目背景图片",
-    "type": 1, // 0-public公开,1-private私有
-    "url": "项目地址",
-    "fork": 11,
+    "url": "https://github.com/createor/ChatGPT",
+    "fork": 0,
     "star": 0
 },{
-    "name": "测试项目2",
-    "desc": "项目描述",
-    "bg-img": "项目背景图片",
+    "name": "百度翻译插件",
+    "desc": "谷歌浏览器扩展插件--百度翻译",
+    "bg_img": "./extensions.png",
     "type": 1, // 0-public公开,1-private私有
-    "url": "项目地址",
-    "fork": 11,
+    "url": "https://github.com/createor/translate_extensions",
+    "fork": 0,
+    "star": 0
+},{
+    "name": "股票桌面端",
+    "desc": "使用python的PyQt库开发的实时监测股票的桌面端程序",
+    "bg_img": "./gupiao.png",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "https://github.com/createor/gupiao",
+    "fork": 0,
     "star": 0   
+},{
+    "name": "监控键盘输入",
+    "desc": "使用python监控键盘输入,过滤/禁止发送关键词汇",
+    "bg_img": "./keyboard.png",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "https://github.com/createor/monitorKeyboard",
+    "fork": 0,
+    "star": 0    
+},{
+    "name": "采集PL/SQL使用的sql命令",
+    "desc": "使用golang编写的记录PL/SQL客户端发送的sql命令",
+    "bg_img": "./oracle.png",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "https://github.com/createor/oracle_syslog",
+    "fork": 0,
+    "star": 0   
+},{
+    "name": "手机自动签到",
+    "desc": "使用python编写的通过adb自动操作手机签到",
+    "bg_img": "./auto.png",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "https://github.com/createor/autoRegister",
+    "fork": 0,
+    "star": 0  
+},{
+    "name": "服务器信息收集",
+    "desc": "使用golang编写的收集服务器信息并上报告警",
+    "bg_img": "./server.png",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "https://github.com/createor/monitor",
+    "fork": 0,
+    "star": 0  
 }]
