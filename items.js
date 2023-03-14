096f755d483169e5b46cb34cae7aa9d1f6f30428
createor <40543429+createor@users.noreply.github.com>
2023-03-14 09:31:28 +0800

    Add files via upload

---
+++ items.js
@@ -0,0 +1,28 @@
+/*
+项目列表
+*/
+items = [{
+    "name": "ChatGPT",
+    "desc": "使用python语言对接chatgpt的api实现的网页版",
+    "bg-img": "项目背景图片",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "https://github.com/createor/monitorKeyboard",
+    "fork": 100,
+    "star": 4
+},{
+    "name": "测试项目2",
+    "desc": "项目描述9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999",
+    "bg-img": "项目背景图片",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "项目地址",
+    "fork": 11,
+    "star": 0
+},{
+    "name": "测试项目2",
+    "desc": "项目描述",
+    "bg-img": "项目背景图片",
+    "type": 1, // 0-public公开,1-private私有
+    "url": "项目地址",
+    "fork": 11,
+    "star": 0   
+}]
